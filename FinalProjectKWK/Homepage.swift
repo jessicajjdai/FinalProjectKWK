7bc63f5025c5c43999d2d0a2f756f8a220ba16f8
jessica <jjdaichen@gmail.com>
2020-07-29 20:23:59 -0400

    Commit - Wed 8:21 PM EST 7/29/2020 - Jessica(added a teeny bit of the homepage storyboard, added a friend on rewards page)

---
+++ FinalProjectKWK/Homepage.swift
@@ -0,0 +1,21 @@
+//
+//  Whatever.swift
+//  FinalProjectKWK
+//
+//  Created by Mimi Zhang on 7/29/20.
+//  Copyright © 2020 Jessica Dai. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController : UIViewController {
+    @IBOutlet weak var numGoalStatus: UILabel!
+    
+    override func viewDidLoad() {
+           super.viewDidLoad()
+
+           // Do any additional setup after loading the view.
+       }
+    
+    
+}
